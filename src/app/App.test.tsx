0be95b6f6418f599b669e7659e3d74ac52a22ae4
Mészöly Márton <meszoly.marton@gmail.com>
2025-06-29 20:33:38 +0200

    add test to App.test.tsx

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react"
 import App from "./App"
+import { initialSettings } from "./slice"
 
 jest.mock("./components/card/GameCard", () => () => <div>Mocked GameCard</div>)
 jest.mock("./components/header/Header", () => () => <div>Mocked Header</div>)
@@ -10,4 +11,10 @@ test("renders mocked components without crashing with SVG and SCSS imports", asy
   const mockedHeader = await screen.findAllByText("Mocked Header")
   expect(mockedGameCards.length).toBeGreaterThan(0)
   expect(mockedHeader.length).toBeGreaterThan(0)
+})
+
+test("renders the initial amount of GameCard components", async () => {
+  render(<App />)
+  const mockedGameCards = await screen.findAllByText("Mocked GameCard")
+  expect(mockedGameCards.length).toEqual(initialSettings.numberOfCards)
 })
