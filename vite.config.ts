49c1c433a96c17df0c8013845397aebdc0a5dfd2
Mészöly Márton <meszoly.marton@gmail.com>
2025-10-11 10:38:27 +0200

    add github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,32 @@
 import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 import svgr from "vite-plugin-svgr"
-import { defineConfig } from "vitest/config"
 
 export default defineConfig({
-  root: "src",
   plugins: [react(), svgr()],
+  base: "/memory-game/",
   build: {
-    outDir: "../build",
-    emptyOutDir: true
+    outDir: "build",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: "static/assets/[name].[hash].js",
+        chunkFileNames: "static/assets/[name].[hash].js",
+        assetFileNames: (assetInfo) => {
+          const ext = assetInfo.names?.[0]
+          if (
+            ["png", "jpg", "jpeg", "gif", "svg", "ico", "webp"].includes(
+              ext ?? ""
+            )
+          ) {
+            return "static/images/[name].[hash][extname]"
+          }
+          if (["css"].includes(ext ?? "")) {
+            return "static/assets/[name].[hash][extname]"
+          }
+          return "static/assets/[name].[hash][extname]"
+        }
+      }
+    }
   }
 })
